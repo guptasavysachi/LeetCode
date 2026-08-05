d7823a89112a68bce615e568cd619ee319e87f1a
Savyasachi Gupta <ssg29may31dec@gmail.com>
2026-08-05 22:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (33.78%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int k=0;
+        for(int i=s.length()-1;i>=0;k++,i--)
+            if(s.charAt(i)==' ')
+                break;
+        return k;
+    }
+}
