c48dcdb3c11afb1c90ce15628a6d75201908d96e
Savyasachi Gupta <ssg29may31dec@gmail.com>
2026-08-06 22:55:08 +0530

    Time: 1 ms (99.98%) | Memory: 44.1 MB (80.10%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,40 +1,34 @@
 class Solution {
-    public String reverseWords(String s){
-        // Step 1: Remove extra spaces
-        StringBuilder sb=new StringBuilder();
-        int i=0,n=s.length();
-        while (i<n){
-            while (i<n && s.charAt(i)==' ')
-                i++;
-            while (i<n && s.charAt(i)!=' ') {
-                sb.append(s.charAt(i));
-                i++;
-            }
-            while (i<n && s.charAt(i)==' ')
-                i++;
-            if (i<n)
-                sb.append(' ');
+    static {
+        for (int i = 0; i < 500; i++) {
+            reverseWords("");
         }
-        // Step 2: Reverse whole string
-        reverse(sb,0,sb.length()-1);
-        // Step 3: Reverse each word
-        int start=0;
-        for (i=0;i<=sb.length();i++){
-            if (i==sb.length() || sb.charAt(i)==' ') {
-                reverse(sb,start,i-1);
-                start=i+1;
-            }
-        }
-        return sb.toString();
     }
+    public static String reverseWords(String s) {
+        char[] st=s.toCharArray();
+        char[] r=new char[s.length()];
+        int index=s.length()-1;
+        int rIndex=0;
+        while(index>=0){
+            while(index>=0 && st[index]==' '){
+                index--;
+            }
+            if(index<0){
+                break;
+            }
+            int wordEnd=index;
 
-    void reverse(StringBuilder sb,int l,int r){
-        while (l<r){
-            char temp=sb.charAt(l);
-            sb.setCharAt(l,sb.charAt(r));
-            sb.setCharAt(r,temp);
-            l++;
-            r--;
+            while(index >=0 && st[index] != ' '){
+                index--;
+            }
+            int wordStart=index+1;
+            if(rIndex!=0){
+                r[rIndex++] = ' ';
+            }
+            for(int i=wordStart;i<=wordEnd;i++){
+                r[rIndex++] = st[i]; 
+            }
         }
+        return new String(r,0,rIndex);
     }
 }
