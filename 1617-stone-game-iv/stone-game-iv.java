3993289a6e85e657e49ba1d6edc8785371180eb2
Savyasachi Gupta <ssg29may31dec@gmail.com>
2026-08-10 22:47:23 +0530

    Time: 2 ms (100.00%) | Memory: 42.2 MB (96.34%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,11 +1,17 @@
 class Solution {
-    boolean dp[]=new boolean[100001];
     public boolean winnerSquareGame(int n) {
-        if(n==0) return false;
-        if(dp[n]) return true;
-        for(int i=1;i*i<=n;i++)
-            if(winnerSquareGame(n-i*i)==false)
-                return dp[n]=true;
-        return dp[n]=false;
+        boolean[] dp = new boolean[n + 1];
+
+        for(int i = 0; i <= n; i++) {
+            if(!dp[i]) {
+                for(int j = 1; i + j * j <= n; j++) {
+                    dp[i + j * j] = true;
+                }
+                if(dp[n]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
