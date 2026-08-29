289dcf19f0de39599d4d647effd162308b0ba120
Savyasachi Gupta <ssg29may31dec@gmail.com>
2026-08-29 19:57:45 +0530

    Time: 20 ms (66.60%) | Memory: 48.3 MB (33.77%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char op = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c))
+                num = num * 10 + (c - '0');
+            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
+                if (op == '+')
+                    stack.push(num);
+                else if (op == '-')
+                    stack.push(-num);
+                else if (op == '*')
+                    stack.push(stack.pop() * num);
+                else if (op == '/')
+                    stack.push(stack.pop() / num);
+                op = c;
+                num = 0;
+            }
+        }
+        int ans = 0;
+        for (int x : stack)
+            ans += x;
+        return ans;
+    }
+}
